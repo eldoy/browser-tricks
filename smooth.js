3e4374199fbefc4303ebddc7fb85c80002f0a18c
fugroup <vidar@fugroup.net>
2019-01-09 07:09:21 +0100

    Add smoother

---
+++ smooth.js
@@ -0,0 +1,34 @@
+const smooth = (el) => {
+  if (typeof el === 'string') {
+    el = document.querySelector(el)
+  }
+  if (!el) {
+    return
+  }
+  const STEP = 40
+  const FAST = 80
+  const LONG = 320
+  const SLOW = 10
+  const OFFSET = 10
+  const move = () => {
+    const y = window.scrollY
+    const top = el.offsetTop - OFFSET
+    if (y !== top) {
+      const diff = Math.abs(y - top)
+      const step = diff > LONG
+        ? FAST
+        : diff < STEP
+          ? SLOW
+          : STEP
+      if (diff < step) {
+        window.scroll(0, top)
+      } else {
+        const i = y > top ? -step : step
+        window.scroll(0, y + i)
+      }
+      window.requestAnimationFrame(move)
+    }
+  }
+  window.requestAnimationFrame(move)
+}
+export default smooth
